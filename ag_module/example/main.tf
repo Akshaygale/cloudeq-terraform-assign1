cfb60f85088bcdecccdc50ede795ea37a658f1b6
Akshaygale <akshaygale19@gmail.com>
2022-08-29 22:07:57 +0530

    terraform3

--- ag_module/example/main.tf
+++ ag_module/example/main.tf
@@ -6,10 +6,10 @@ module "aws_iam_users" {
 }
 
 
-# module "my-bucket-module" { /* It create 1 to add */
-#   source      = "../../s3_bucket"
-#   bucket_name = var.bucket_name
-# }
+module "my-bucket-module" { /* It create 1 to add */
+  source      = "../../s3_bucket"
+  bucket_name = var.bucket_name
+}
 
 module "my_aws_instance" { /* It create 2 to add */
   source    = "../../ec2"
--- s3_bucket/main.tf
+++ s3_bucket/main.tf
@@ -1,8 +1,8 @@
-# resource "aws_s3_bucket" "my-bucket" {
-#   bucket = var.bucket_name
+resource "aws_s3_bucket" "my-bucket" {
+  bucket = var.bucket_name
 
-#   tags = {
-#     Name        = "Akshay dummy bucket"
-#     Environment = "Dev"
-#   }
-# }
+  tags = {
+    Name        = "Akshay dummy bucket"
+    Environment = "Dev"
+  }
+}
